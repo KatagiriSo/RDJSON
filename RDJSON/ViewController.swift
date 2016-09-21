8d526deb0b38672e63da7cf1812f7ca1a1974f3a
katagiri <so@katagiri>
2016-09-21 09:43:45 +0900

    Initial Commit

---
+++ RDJSON/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RDJSON
+//
+//  Created by 片桐奏羽 on 2016/09/21.
+//  Copyright © 2016年 Rodhos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
